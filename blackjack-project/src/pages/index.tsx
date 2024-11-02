42398f3b9f96358f4585990389ebd0c6e8438cca
uuizdashdw <chany920@gmail.com>
2024-11-02 17:24:52 +0900

    index.tsx 정리

--- blackjack-project/src/pages/index.tsx
+++ blackjack-project/src/pages/index.tsx
@@ -27,37 +27,19 @@ import {
 } from '@/store/features/gameSlice';
 
 // Type
-import { CardData } from '@/types/card';
 import MetaData from '@/components/meta/MetaData';
 
 export default function Home() {
+	const dispatch = useDispatch();
+
 	const [splashPage, setSplashPage] = useState(true);
 	const shuffledCards = useSelector(
 		(state: RootState) => state.game.shuffledCards,
 	);
 	const cardsLeft = useSelector((state: RootState) => state.game.cardsLeft);
 	const beginRound = useSelector((state: RootState) => state.game.beginRound);
-	const soundMuted = useSelector(
-		(state: RootState) => state.soundMuted.soundMuted,
-	);
-	const currentBankTotal = useSelector(
-		(state: RootState) => state.game.currentBankTotal,
-	);
-	const bet = useSelector((state: RootState) => state.game.bet);
-	const chipsInBank = useSelector((state: RootState) => state.game.chipsInBank);
-	const dealerHand = useSelector((state: RootState) => state.game.dealerHand);
-	const playerHand = useSelector((state: RootState) => state.game.playerHand);
-	const dealerTotal = useSelector((state: RootState) => state.game.dealerTotal);
-	const playerTotal = useSelector((state: RootState) => state.game.playerTotal);
-	const offerDoubleDown = useSelector(
-		(state: RootState) => state.game.offerDoubleDown,
-	);
-	const offerSplitHand = useSelector(
-		(state: RootState) => state.game.offerSplitHand,
-	);
-	const endRound = useSelector((state: RootState) => state.game.endRound);
 
-	const dispatch = useDispatch();
+	const endRound = useSelector((state: RootState) => state.game.endRound);
 
 	const { shuffleCards } = useShuffleCards();
 
